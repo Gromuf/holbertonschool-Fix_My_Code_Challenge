f211da0877f02da4619ad755216eb53e8a37da40
Gromuf <luigi.beaumois@gmail.com>
2025-01-14 10:29:32 +0100

    task 1

--- challenge/1-print_square.js
+++ challenge/1-print_square.js
@@ -13,13 +13,11 @@ if (process.argv.length <= 2) {
   process.exit(1);
 }
 
-const size = parseInt(process.argv[2]);
-
-if (isNaN(size) || size <= 0) {
-  process.stderr.write("Invalid size. Please provide a positive integer.\n");
-  process.exit(1);
-}
+size = parseInt(process.argv[2]);
 
 for (let i = 0; i < size; i++) {
-  console.log("#".repeat(size));
+  for (let j = 0; j < size; j++) {
+    process.stdout.write("#");
+  }
+  process.stdout.write("\n");
 }
